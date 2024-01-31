7e3c2c9b98375eb3b71971fcad8d1a180b9f28ba
0001003 <158238077+0001003@users.noreply.github.com>
2024-01-31 10:48:09 +0900

    Add files via upload

---
+++ day 01/var.py
@@ -0,0 +1,30 @@
+#var
+#variable - 변수
+# myname = input("너의 이름은?")
+# age = 10 + 20
+# print(myname)
+# print(age)
+
+
+
+
+# 1. 당신의 이름?
+# 2. 당신의 나이?
+# 3. 당신의 최애 음식
+# 4. 당신의 최애 유투버는?
+
+myname = input("당신의 이름?")
+print(myname)
+age = input("당신의 나이?")
+print(age)
+favorite_food = input("당신의 최애 음식?")
+print(favorite_food)
+favorite_youtuber = input("당신의 최애 유투버?")
+print(favorite_youtuber)
+
+
+
+
+
+
+
